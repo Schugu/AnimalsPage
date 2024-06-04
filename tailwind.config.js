7aaa4ed091660bbc962a6fef09ff5dc67af91ceb
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-04 18:48:47 -0300

    Add: Plugin de iconos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+import { addDynamicIconSelectors } from '@iconify/tailwind';
+
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,6 +9,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    addDynamicIconSelectors()
+  ],
+};
 
+export default config;
