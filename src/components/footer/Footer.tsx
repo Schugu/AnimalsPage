e0146cb9f027931a0e6503bc84c8e3c952669c91
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-05 18:50:11 -0300

    Add: Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,23 @@
-
+import LinkSection from "../LinkSection.tsx";
 export default function Footer() {
   return (
-    <article>
-      Footer
-    </article>
+    <footer className="flex flex-wrap justify-between items-center p-2.5 gap-2
+    bg-gray-400 text-xl">
+
+
+      <div className="flex flex-wrap gap-1 justify-center items-center">
+        <span className="icon-[ic--baseline-copyright]" />
+        <p>2024</p>
+      </div>
+
+      <LinkSection
+        linkTo="#header"
+        icono="icon-[ic--baseline-arrow-upward]"
+        label="Volver arriba"
+      />
+
+      <p className="">leo.schugu@gmail.com</p>
+
+    </footer>
   )
 }
