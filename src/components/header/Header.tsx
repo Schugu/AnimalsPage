adc2d7de3e471b0f3374f8e09b9bcdb63bc7e6a7
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-05 18:48:49 -0300

    Refactor: Cambios de etiquetas

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,8 @@
-import LinkSection from "./LinkSection.tsx";
+import LinkSection from "../LinkSection.tsx";
 export default function Header() {
   return (
-    <article className="flex flex-wrap justify-between items-center p-2.5 gap-2
-    bg-red-200">
+    <header className="flex flex-wrap justify-between items-center p-2.5 gap-2
+    bg-red-200" id="header">
       <a href="#" className="flex justify-center items-center gap-1 text-3xl">
         <span className="icon-[mdi--paw]" />
         <p className="font-bold">Animales</p>
@@ -13,6 +13,6 @@ export default function Header() {
         <LinkSection linkTo="#" icono="icon-[ic--baseline-person-add]" label="Registrarse" />
         <LinkSection linkTo="#" icono="icon-[ic--baseline-account-circle]" label="Acceder" />
       </div>
-    </article>
+    </header>
   )
 }
