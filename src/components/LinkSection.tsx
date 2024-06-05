e30183ea3ddb14602236e47e534e7a3c780bf260
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-05 18:49:43 -0300

    Refactor: Cambio de ficheros

--- src/components/LinkSection.tsx
+++ src/components/LinkSection.tsx
@@ -3,13 +3,12 @@ type LinkSectionProps = {
   linkTo: string,
   icono: string,
   label: string,
-
 }
 
 export default function LinkSection({linkTo, icono, label}: LinkSectionProps) {
   return (
     <a href={linkTo} className="flex justify-center items-center gap-1 
-    hover:text-red-50 hover:drop-shadow-sm transition duration-15000 ease-in-out">
+    hover:text-red-50 hover:drop-shadow-sm transition duration-15000 ease-in-out select-none">
       <span className={`${icono}`} />
       <p>{label}</p>
     </a>
