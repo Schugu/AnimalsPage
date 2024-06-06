d1d323f53d3024659547934f524c5fe3ef7e17ca
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-06 09:31:54 -0300

    Add: Animación

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './card.css'; // Asegúrate de importar tu archivo CSS
 
 type CardProps = {
   nombre: string;
@@ -8,30 +9,31 @@ type CardProps = {
 }
 
 export default function Card({ nombre, link, parrafo }: CardProps) {
-  const [isHidden, setIsHidden] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
   return (
-    <article onClick={() => { setIsHidden(!isHidden) }}
-      className="flex flex-col justify-between text-center w-72 h-72 shadow-md rounded-lg bg-[#f2f2f2] cursor-pointer hover:bg-gray-200">
-      <div className={`w-full h-full p-2.5 flex flex-col gap-2 items-center justify-between ${isHidden ? 'hidden' : ''}`}>
-        <section className="w-full h-4/6">
-          <img className="w-full h-full object-cover object-center rounded-lg" src={`/ImgAnimales/${nombre}.jpg`} alt={nombre} />
-        </section>
-        <section className="w-full h-2/6 flex justify-center items-center">
-          <p className="w-fit text-[#f79a8e] text-4xl drop-shadow">
-            {nombre}
+    <article onClick={() => { setIsFlipped(!isFlipped) }}
+      className="card-container">
+      <div className={`card ${isFlipped ? 'flipped' : ''} shadow cursor-pointer`}>
+        <div className="front w-full h-full p-2.5 flex flex-col gap-2 items-center justify-between">
+          <section className="w-full h-4/6">
+            <img className="w-full h-full object-cover object-center rounded-lg" src={`/ImgAnimales/${nombre}.jpg`} alt={nombre} />
+          </section>
+          <section className="w-full h-2/6 flex justify-center items-center">
+            <p className="w-fit text-[#f79a8e] text-4xl drop-shadow">
+              {nombre}
+            </p>
+          </section>
+        </div>
+
+        <div className="back w-full h-full p-2.5 flex flex-col gap-2 items-center">
+          <a className="w-full text-center text-[#f79a8e] text-2xl hover:bg-red-50 rounded" href={link} target="_blank">
+            <p className="border-b-2 border-gray-700">{nombre}</p>
+          </a>
+          <p className="text-black overflow-auto text-balance">
+            {parrafo}
           </p>
-        </section>
-      </div>
-
-      <div className={`w-full h-full p-2.5 flex flex-col gap-2 items-center justify-between ${!isHidden ? 'hidden' : ''}`}>
-        <a className="w-full text-[#f79a8e] text-2xl hover:bg-red-50 rounded" href={link} target="_blank">
-          <p className="border-b-2 border-gray-700">{nombre}</p>
-        </a>
-
-        <p className="text-black overflow-auto">
-          {parrafo}
-        </p>
+        </div>
       </div>
     </article>
   )
